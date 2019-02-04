ba18a1613c6414420cd1b881437c889b221d2f7f
shariq95 <shariq.syed31@gmail.com>
2019-02-04 22:36:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar.js";
+import "./App.scss";
+import TourList from "./components/TourList";
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Navbar />
+        <TourList />
+      </main>
+    );
+  }
+}
+
+export default App;
